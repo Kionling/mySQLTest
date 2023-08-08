b2ca36c1cc38eaf6dae1c9d4ca9d449976280dc1
kionling <djvdjv209@gmail.com>
2023-08-07 23:58:07 -0700

    Created controllers script to handle all requests to the API and also created database structure

---
+++ controllers/pcparts_controller.js
@@ -0,0 +1,56 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var pcpart = require("../models/pcparts.js");
+
+
+router.get("/", function(req, res){
+    pcpart.all(function(data){
+        var hbsObject = {
+            pcparts: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/pcparts", function(req, res) {
+    pcpart.create(["name", "pcpart"], [req.body.name, req.body.pcpart], function(result) {
+        res.json({ id: result.insertId });
+    })
+})
+
+router.put("/api/pcparts/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    pcpart.update(
+        {
+            pcpart: req.body.pcpart
+        },
+        condition,
+        function(result) {
+            if(result.changedRows === 0) {
+                return res.status(404),end();
+            }
+            res.status(200).end();
+        }
+    )
+});
+
+router.delete("/api/pcparts/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    pcpart.delete(condition, function(result) {
+        if(result.affectedRows === 0) {
+            return res.status(404).end();
+        } else {
+            return res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
