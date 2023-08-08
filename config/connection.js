43c39d4e74b4bc6127006811e4d2fc202ceca3fb
kionling <djvdjv209@gmail.com>
2023-08-08 08:54:05 -0700

    Installed mySQL2 as the previous mySQL library was not working to connect to the local database. Updated the server configuration to handle the front-end views folder. As well as created the models.js script to handle interactions with the database

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,51 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
+// // Set up MySQL connection.
+// var mysql = require("mysql2");
 
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "pcpartsDB"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "pcpartsDB"
+// });
 
-// for Heroku
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+// // for Heroku
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+// // Make connection.
+// connection.connect(function (err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// // Export connection for our ORM to use.
+// module.exports = connection;
+
+
+
+var mysql = require("mysql2");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  // For Heroku deployment
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // For local development
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "pcpartsDB"
+  });
+}
 
 // Make connection.
-connection.connect(function (err) {
+connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
--- server.js
+++ server.js
@@ -1,5 +1,37 @@
+// var express = require("express");
+// const exphbs = require('express-handlebars');
+
+// var PORT = process.env.PORT || 8080;
+
+// var app = express();
+
+// // Serve static content for the app from the "public" directory in the application directory.
+// app.use(express.static("public"));
+
+// // Parse application body
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+
+// app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main"}));
+
+
+
+// // Import routes and give the server access to them.
+// var routes = require("./controllers/pcparts_controller.js");
+
+// app.use(routes);
+
+// // Start our server so that it can begin listening to client requests.
+// app.listen(PORT, function() {
+//   // Log (server-side) when our server has started
+//   console.log("Server listening on: http://localhost:" + PORT);
+// });
+
+
 var express = require("express");
 const exphbs = require('express-handlebars');
+const path = require('path'); // Make sure to include this
 
 var PORT = process.env.PORT || 8080;
 
@@ -12,9 +44,9 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// Configure view engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.handlebars', exphbs.engine({ extname: '.handlebars', defaultLayout: "main"}));
 app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them.
